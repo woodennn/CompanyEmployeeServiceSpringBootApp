89c6020e83b083410e391c3cb8aef8463e01270b
Ihor Moskalenko <igorechekmos@gmail.com>
2026-05-16 19:47:18 +0200

    -

--- src/test/java/com/example/company_employee_service/CompanyEmployeeServiceApplicationTests.java
+++ src/test/java/com/example/company_employee_service/CompanyEmployeeServiceApplicationTests.java
@@ -2,12 +2,33 @@ package com.example.company_employee_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
 
 @SpringBootTest
 class CompanyEmployeeServiceApplicationTests {
 
+	@TestConfiguration
+	static class TestConfig {
+		@Bean
+		public ClientRegistrationRepository clientRegistrationRepository() {
+			return new InMemoryClientRegistrationRepository(
+					ClientRegistration.withRegistrationId("keycloak")
+							.clientId("dummy")
+							.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+							.redirectUri("http://localhost")
+							.authorizationUri("http://localhost")
+							.tokenUri("http://localhost")
+							.build()
+			);
+		}
+	}
+
 	@Test
 	void contextLoads() {
 	}
-
 }
